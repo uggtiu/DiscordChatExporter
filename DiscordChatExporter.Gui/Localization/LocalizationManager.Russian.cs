d762d26e54a6f7c2acb394cc355d18cdba652397
Daniil <uggtiu@uggtiu.pw>
2026-05-08 15:10:42 +0300

    Some fixes

--- DiscordChatExporter.Gui/Localization/LocalizationManager.Russian.cs
+++ DiscordChatExporter.Gui/Localization/LocalizationManager.Russian.cs
@@ -4,7 +4,7 @@ namespace DiscordChatExporter.Gui.Localization;
 
 public partial class LocalizationManager
 {
-    private static readonly IReadOnlyDictionary<string, string> EnglishLocalization =
+    private static readonly IReadOnlyDictionary<string, string> RussianLocalization =
         new Dictionary<string, string>
         {
             // Dashboard
